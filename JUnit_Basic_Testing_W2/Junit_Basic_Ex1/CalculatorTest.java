42d8ebf60e6f9afe45ddfe7e4da362ec6dd86542
Sagnik Raha <69630418+sagnik-004@users.noreply.github.com>
2025-06-27 17:50:18 +0530

    JUnit Testing files uploaded.

---
+++ JUnit_Basic_Testing_W2/Junit_Basic_Ex1/CalculatorTest.java
@@ -0,0 +1,10 @@
+package org.example;// File: src/test/java/CalculatorTest.java
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+    @Test
+    public void dummyTest() {
+        assertTrue(true);
+    }
+}
---
+++ JUnit_Basic_Testing_W2/Junit_Basic_Ex4/Calculator.java
@@ -0,0 +1,12 @@
+package org.example;
+
+// File: src/main/java/Calculator.java
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
